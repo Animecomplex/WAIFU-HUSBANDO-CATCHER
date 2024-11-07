57356235406c9d542b8d7b17bd80b01c4a77be39
Anime Sparta <suryagupta1928@gmail.com>
2024-11-07 17:00:52 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,18 +2,18 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "6765826972"
-    sudo_users = "6845325416", "6765826972"
-    GROUP_ID = -1002133191051
-    TOKEN = "6707490163:AAHZzqjm3rbEZsObRiNaT7DMtw_i5WPo_0o"
+    OWNER_ID = "2061656269"
+    sudo_users = "2061656269"
+    GROUP_ID = -1002237016385
+    TOKEN = "7248959755:AAFjDhuxqoOIw0492tYbk7BCu4wpN9I-nCQ"
     mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
     SUPPORT_CHAT = "Collect_em_support"
     UPDATE_CHAT = "Collect_em_support"
-    BOT_USERNAME = "Collect_Em_AllBot"
-    CHARA_CHANNEL_ID = "-1002133191051"
-    api_id = 26626068
-    api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
+    BOT_USERNAME = "Ekdkekdekdke_bot"
+    CHARA_CHANNEL_ID = "-1002237016385"
+    api_id = 20478011
+    api_hash = "0e4dcf39643e83c3c174a0d2370e5b4a"
 
     
 class Production(Config):
